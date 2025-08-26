052dbbf41f8cb9485a9628116fc2662b08bff50c
Advay Singh <advaysingh2012@gmail.com>
2025-08-26 14:46:40 +0000

    TaskBot AI App is now avalible!

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("android_link").addEventListener("click", ()=>{
+    window.alert("Thanks for downloading TaskBot AI!. Download will start shortly. If is doesn't start, please click the link again.");
+    document.getElementById("android_logo").style.display = "none"
+    document.getElementById("install_text").textContent = "Downloaded"
+})
